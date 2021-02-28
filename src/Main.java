38a55c985f8cfe757a5755ca84d5dd8e2feecb9d
짠종 <jjanjong@jjanjong.local>
2021-02-28 17:46:48 +0900

    thread

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main{
+
+    public static void main(String[] args){
+
+        System.out.println("Hello World!");
+        ThreadProgram tp = new ThreadProgram();
+        tp.main(50);
+    }
+
+}
