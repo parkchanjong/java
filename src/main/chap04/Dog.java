b94ddb89327508abc79f24d781e3a56867da1d99
parkchanjong <7000cj@gmail.com>
2021-05-05 22:41:24 +0900

    [chap04] 메소드에서 인스턴스 변수 사용
    - 객체의 행동

---
+++ src/main/chap04/Dog.java
@@ -0,0 +1,18 @@
+package main.chap04;
+
+public class Dog {
+    int size;
+    String name;
+
+    void bark() {
+        if (size > 60) {
+            System.out.println("Wooof! Wooof!");
+
+        } else if (size > 14) {
+            System.out.println("Ruff! Ruff!");
+        } else {
+            System.out.println("Yip! Yip!");
+        }
+    }
+
+}
---
+++ src/main/chap04/PoorDog.java
@@ -0,0 +1,12 @@
+package main.chap04;
+
+public class PoorDog {
+    private int size;
+    private String name;
+    public int getSize() {
+        return size;
+    }
+    public String getName() {
+        return name;
+    }
+}
