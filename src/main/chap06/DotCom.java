1ecd839905a4c1ef27da30b5a0652121d0e59f13
parkchanjong <7000cj@gmail.com>
2021-05-10 22:11:15 +0900

    [chap06] 자바 api
    - ArraryList 사용해 게임완성

---
+++ src/main/chap06/DotCom.java
@@ -0,0 +1,34 @@
+package main.chap06;
+
+import java.util.ArrayList;
+
+public class DotCom {
+    private ArrayList<String> locationCells;
+
+    public void setLocationCells(ArrayList<String> loc)
+    {
+        locationCells = loc;
+    }
+
+    public String checkYourself(String userInput)
+    {
+        String result = "miss";
+        int index = locationCells.indexOf(userInput);
+        if (index >= 0) {
+            locationCells.remove(index);
+            if (locationCells.isEmpty()) {
+                result = "kill";
+            }
+            else
+            {
+                result = "hit";
+            }
+        }
+        return result;
+    }
+
+    private String name;
+    public void setName(String string) {
+        name = string;
+    }
+}
